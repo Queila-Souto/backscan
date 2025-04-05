42ce5fdbadcfe481b096758b7d80b636f5ffa09a
Queila <queilamonteiro@msn.com>
2025-04-05 20:35:43 -0300

    config credenciais

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "7312800220:AAGrpQYMFWi7ih3YJuZdTHWeT_DqeDf9xo4"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "1449961930"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
