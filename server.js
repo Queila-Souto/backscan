b6955c6c86bb8f8dd1140bd9e959dbe26a99fe7f
Queila <queilamonteiro@msn.com>
2025-04-05 21:53:23 -0300

    config main route

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(__dirname));
 
 const TELEGRAM_BOT_TOKEN = "7312800220:AAGrpQYMFWi7ih3YJuZdTHWeT_DqeDf9xo4"; // Substitua pelo token do seu bot
 const TELEGRAM_CHAT_ID = "1449961930"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+
+// Rota principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
